3355a2c54731a397faa377b8263d23431026bb70
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-16 10:34:11 -0700

    Added laser_bots.rb to include multi level module and classes

---
+++ laser_bots.rb
@@ -0,0 +1,30 @@
+module LaserBots
+    module Console
+        class Command
+        end
+    end
+    
+    module World
+        class Player
+            attr_reader :name
+            def initialize(name)
+                @name = name
+            end
+        end
+
+        class Robot
+            attr_reader :name
+            def initialize(name)
+                @name = name
+            end
+        end
+
+        def target_player(name)
+            Player.new(name)
+        end
+    end
+end
+
+player = LaserBots::World::Player.new("Jason")
+
+puts player.name

a4882a3e552925c2f10ff094a97bc7ef89060c91
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-23 21:55:07 -0700

    Added stock report method

--- store.rb
+++ store.rb
@@ -1,8 +1,58 @@
 module Inventoryable
     
+    def self.included(klass)
+        klass.extend(ClassMethods)
+        klass.extend(Enumerable)
+    end
+
+    module ClassMethods
+        def create(attributes)
+            object = new(attributes)
+            instances.push(object)
+            return object
+        end
+
+        def instances
+            @instances ||= []
+        end
+
+        def each(&block)
+            instances.each(&block)
+        end
+
+        def in_stock_report
+            puts "#{self.to_s} In Stock Report"
+            reportable = instances.select{ |instance| instance.in_stock? }
+            reportable.each do |item|
+                line = []
+                line.push("Item: #{item.attributes[:name]}")
+                line.push("Stock: #{item.stock_count}")
+                if item.attributes.include?(:size)
+                    line.push("Size: #{item.attributes[:size]}")
+                end
+                puts line.join("\t")
+            end
+            puts "\n"
+        end
+    end
+
+
+    def stock_count
+        @stock_count ||= 0
+    end
+
+    def stock_count=(number)
+        @stock_count = number
+    end
+
+    def in_stock?
+        stock_count > 0
+    end
 end
 
+
 class Shirt
+    include Inventoryable
     attr_accessor :attributes
 
     def initialize(attributes)
@@ -10,7 +60,9 @@ class Shirt
     end
 end
 
+
 class Pant
+    include Inventoryable
     attr_accessor :attributes
 
     def initialize(attributes)
@@ -19,6 +71,7 @@ class Pant
 end
 
 class Accessory
+    include Inventoryable
     attr_accessor :attributes
 
     def initialize(attributes)
@@ -27,5 +80,31 @@ class Accessory
 end
 
 
-shirt = Shirt.new(name: "MTF", size: "L")
-shirt2 = Shirt.new(name: "MTF", size: "M")
+shirt = Shirt.create(name: "MTF", size: "L")
+shirt.stock_count = 10
+
+shirt = Shirt.create(name: "MTF2", size: "L")
+
+shirt = Shirt.create(name: "MTF", size: "M")
+shirt.stock_count = 9
+
+pant = Pant.create(name: "Jeans", size: "M")
+pant.stock_count = 2
+
+pant = Pant.create(name: "Jeans", size: "S")
+pant.stock_count = 4
+
+accessory = Accessory.create(name: "Belt", size: "M")
+accessory.stock_count = 1
+
+accessory = Accessory.create(name: "Belt", size: "L")
+accessory.stock_count = 1
+
+accessory = Accessory.create(name: "Necklace")
+accessory.stock_count = 1
+
+Shirt.in_stock_report
+Pant.in_stock_report
+Accessory.in_stock_report
+
+Shirt.in_stock_report
